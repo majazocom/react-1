de818f1726880177aa1b3b2fa283655e1ed008e0
majazocom <maja.gedda@zocom.se>
2021-02-22 10:28:53 +0100

    init repo

---
+++ script.js
@@ -0,0 +1,16 @@
+
+class Hello extends React.Component {
+    render() {
+        return (
+            <section>
+                {/* kommentar */}
+                <h1>Hello World</h1>
+                <p>Jag heter Maja</p>
+                <form>
+                    <input placeholder="text" />
+                </form>
+            </section>
+        )
+    }
+}
+ReactDOM.render(<Hello />, document.getElementById('app'));
