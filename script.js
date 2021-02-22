1b6633c8eb2472837b5a5afa76b95b50f110dc4f
majazocom <maja.gedda@zocom.se>
2021-02-22 11:36:15 +0100

    changes

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 
-// class Hello extends React.Component {
-//     render() {
-//         return (
-//             <section>
-//                 {/* kommentar */}
-//                 <h1 class="hejhej">Hello World</h1>
-//                 <p>Jag heter Maja</p>
-//                 <form>
-//                     <input placeholder="text" />
-//                 </form>
-//             </section>
-//         )
-//     }
-// }
-// ReactDOM.render(hello, document.getElementById('app'));
+
+class Hello extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            name: 'Maja',
+            age: '32'
+        }
+    }
+    render() {
+        return (
+            <section>
+                {/* kommentar */}
+                <h1 className="hejhej">Hello World</h1>
+                <p>Jag heter {this.state.name} och jag är {this.state.age} år gammal</p>
+                <form>
+                    <input placeholder="text" />
+                </form>
+            </section>
+        )
+    }
+}
+ReactDOM.render(<Hello />, document.getElementById('app'));
